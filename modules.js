5f023bdc008c7c8f239d0ed7ca6a3b9e7707010f
Matt Gonzalez <matt@echoaudio.com>
2023-05-17 17:12:43 -0700

    AIO module types, shorter introduction

--- modules.js
+++ modules.js
@@ -2,7 +2,7 @@ var modules =
 [
     [ "Library startup, shutdown, and error handling", "group___library-startup-and-shutdown.html", "group___library-startup-and-shutdown" ],
     [ "Echo AIO discovery and information", "group___a_i_o-inquiry.html", "group___a_i_o-inquiry" ],
-    [ "Windows-specific audio functions", "group___windows-audio-functions.html", "group___windows-audio-functions" ],
+    [ "Windows audio functions", "group___windows-audio-functions.html", "group___windows-audio-functions" ],
     [ "Output channel parameter functions", "group___output-channel-parameter-functions.html", "group___output-channel-parameter-functions" ],
     [ "Module parameter functions", "group___module-parameter-functions.html", "group___module-parameter-functions" ],
     [ "Device parameter functions", "group___device-parameter-functions.html", "group___device-parameter-functions" ],
